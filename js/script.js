740317d685c11b5a9a08512a3fc8be04fe0feab5
DamianoCelesti <damiano.celesti@gmail.com>
2025-01-14 02:22:05 +0100

    added milestone 2

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Elementi di input della pagina
+const percorso = document.getElementById("km");
+const numero = document.getElementById("eta");
+const button = document.querySelector("#conferma");
+
+// const km = percorso.value;
+// const eta = numero.value;
+
+
+// Elementi di output
+
+const etaScelta = document.getElementById("etaScelta");
+const prezzo = document.getElementById("prezzo");
+
+
+button.addEventListener("click", function() {
+
+    const km = percorso.value;
+    const eta = numero.value;
+    
+    console.log(`Distanza: ${km} km`);
+    console.log(`Età selezionata: ${eta}`);
+
+    // formula per vedere gli sconti
+    const prezzoKm = 0.21;
+    let prezzoPieno = km * prezzoKm;
+    console.log('Prezzo pieno', prezzoPieno);
+
+
+    let = prezzoScontato = prezzoPieno;
+    if (eta === "minorenne") {
+        prezzoScontato = prezzoPieno * 0.80;
+    } 
+    else if (eta === "senior") {
+        prezzoScontato = prezzoPieno * 0.60;
+    }
+    else {
+        console.log('Non hai diritto a sconti. Il prezzo pieno è:', prezzoPieno.toFixed(2));
+    }
+
+
+    prezzoFinale = prezzoScontato.toFixed(2);
+    console.log('Il prezzo finale scontato è', prezzoFinale);
+    prezzo.innerHTML = `Il prezzo è: ${prezzoFinale}`;
+    etaScelta.innerHTML = `Eta selezionata: ${eta}`;
+});
